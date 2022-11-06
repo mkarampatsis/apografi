e51abc854145a373e3c8f7014c8556979b6e0391
mkarampatsis <marka@central.ntua.gr>
2022-11-06 23:10:46 +0200

    changes to organizations

--- models/Logs.py
+++ models/Logs.py
@@ -1,6 +1,8 @@
 from mongoengine import *
+from datetime import datetime
 
 class Logs(DynamicDocument):
   data = DynamicField()
+  timestamp = DateTimeField(required=True, default=datetime.utcnow)
 
   meta = {"db_alias": "ministryDB"}

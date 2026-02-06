6ffde75484152a310c1cd28982239f2c2e185b30
mkarampatsis <marka@central.ntua.gr>
2026-02-06 11:18:24 +0200

    changes to utils and units

--- organizational-units.py
+++ organizational-units.py
@@ -20,6 +20,9 @@ ORGANIZATIONS_URL = f"{API_URL}/public/organizations"
 ORGANIZATION_UNITS_URL = f"{API_URL}/public/organizational-units?organizationCode=%s"
 ORGANIZATION_TREE_URL = f"{API_URL}/public/organization-tree?organizationCode=%s"
 
+units_with_problems = [
+  { "code":"763976", "field":"email", 'problem':'email is tm.sint.td.akiklades@efka.gov.gr  / tm.par.td.akiklades@efka.gov.gr' }
+]
 
 def processOrganizationUnits(code):  
   print(f"  - Συγχρονισμός μονάδων οργανισμού: {code}...")

59c8c25650cc9656521f8f0fc00136e034b2051e
mkarampatsis <marka@central.ntua.gr>
2026-02-21 10:09:59 +0200

    changes to chmod

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avz --delete .env models connection.py dictionaries.py getUnitsPurpose.py organizational-units.py organizations.py utils.py requirements.txt lola:apografi
+rsync -avz --delete .env models connection.py sdad*.py psped*.py utils.py requirements.txt lola:apografi
 ssh lola 'source ~/apografi/venv/bin/activate && pip install -r ~/apografi/requirements.txt'

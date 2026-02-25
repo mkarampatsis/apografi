92c4420286ea9e55cdb76fb81035cb69b411b5ef
mkarampatsis <marka@central.ntua.gr>
2026-02-25 13:19:39 +0200

    change log level to debug for testing purposes

--- models/sdad/synclog.py
+++ models/sdad/synclog.py
@@ -5,7 +5,7 @@ class SyncLog(me.Document):
   meta = {"collection": "synclog", "db_alias": "sdad"}
 
   date = me.DateTimeField(default=datetime.now())
-  entity = me.StringField(required=True, choices=["dictionary", "organization", "organizational-unit"])
+  entity = me.StringField(required=True, choices=["dictionary", "organization", "organizational_unit"])
   action = me.StringField(required=True, choices=["insert", "update"])
   doc_id = me.StringField(required=True)
   value = me.DictField(required=True)
